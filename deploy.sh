6884a7e8640da581dc6934d86545e4a06cd41f56
Chang Yong Lik <theeahlag@gmail.com>
2022-01-17 00:02:58 +0900

    Added CSS

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t cygnetops/multi-client-k8s:latest -t cygnetops/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t cygnetops/multi-server-k8s-pgfix:latest -t cygnetops/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
+docker build -t cygnetops/multi-worker-k8s:latest -t cygnetops/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push cygnetops/multi-client-k8s:latest
+docker push cygnetops/multi-server-k8s-pgfix:latest
+docker push cygnetops/multi-worker-k8s:latest
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push cygnetops/multi-client-k8s:$SHA
+docker push cygnetops/multi-server-k8s-pgfix:$SHA
+docker push cygnetops/multi-worker-k8s:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=cygnetops/multi-server-k8s-pgfix:$SHA
+kubectl set image deployments/client-deployment client=cygnetops/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=cygnetops/multi-worker-k8s:$SHA
